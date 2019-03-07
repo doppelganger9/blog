9a0b9ffda3f36842e6a4ca17987eb084ee6ad1d6
David Lacourt <david.lacourt+github@gmail.com>
2019-03-07 21:30:42 +0000

    Adds plugin to embed twitter statuses

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,6 +43,7 @@ module.exports = {
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
+          `@weknow/gatsby-remark-twitter`,
         ],
       },
     },
