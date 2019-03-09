fafd3ee4476adabd333376ede35aa795a99150c6
David LACOURT <david.lacourt@gmail.com>
2019-03-09 23:03:29 +0100

    Updates URL for website

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     title: `David's Blog`,
     author: `David Lacourt`,
     description: `A developer's blog. Might be useful. Maybe.`,
-    siteUrl: ` https://doppelganger9.github.io/blog/`,
+    siteUrl: ` https://lacourt.dev/`,
     social: {
       twitter: `doppelganger9`,
     },
