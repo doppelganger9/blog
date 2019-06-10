9da9f51dd69fc88eb8252deb45e100d96c0501f9
David Lacourt <david.lacourt@gmail.com>
2019-06-10 11:42:02 +0200

    Adds e2e test for regression testing blog posts + fix missing slug template

---
+++ cypress/integration/blog-post.js
@@ -0,0 +1,34 @@
+import { titleBarShouldBeShown, builtByFooterShouldBeShown, articleFooterShouldBeShown, aBackLinkShouldBeShownTo } from '../common';
+
+describe(`a blog post`, () => {
+  beforeEach(() => {
+    cy.visit('/').get(`h3:contains('Hello')`).find('a').click()
+  });
+
+  it(`should show the title bar in h3`, () => {
+    titleBarShouldBeShown('h3')
+  });
+
+  it(`should have a back link to blog post list/home`, () => {
+    aBackLinkShouldBeShownTo('/')
+  });
+
+  it(`should show the published date in the first <p> following the <h1> article title`, () => {
+    cy.get(`h1 + p`)
+      .should('exist')
+      .should('contain', '2019')
+  });
+
+  it(`should show the time to read in a second <p> following the <h1> article title`, () => {
+    cy.get(`h1 + p + p`).should('contain', 'min read')
+  });
+
+  it(`should show the "written by" footer`, () => {
+    articleFooterShouldBeShown()
+  });
+
+  it(`should show the "built with" footer`, () => {
+    builtByFooterShouldBeShown()
+  });
+
+});
