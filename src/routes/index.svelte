91c9f7e797c9a28d71516db59238be99cb84458a
David LACOURT <david.lacourt@gmail.com>
2019-06-22 22:50:39 +0200

    Fix SEO for index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -40,6 +40,23 @@
 
 <svelte:head>
   <title>David's Blog</title>
+
+  <meta name="description" content="David Lacourt's Blog. Posts about web technology, modern JavaScript frameworks, Agile methodologies, Software Craftmanship." />
+  <meta name="keywords" content="sapper,saper,sappr,svelte,sevlte,svetle,blog,david,dave,lacourt,lacour,la cour,la court,developpeur,developer,daveloper,devloper,devlopr,gatsbyjs,gatsby,gatsbi,gastby"/>
+
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://lacourt.dev/">
+  <meta property="og:title" content="David's Blog">
+  <meta property="og:description" content="David Lacourt's Blog. Posts about web technology, modern JavaScript frameworks, Agile methodologies, Software Craftmanship.">
+  <meta property="og:image" content="https://lacourt.dev/profile-pic.png">
+
+  <!-- Twitter -->
+  <meta property="twitter:card" content="summary_large_image">
+  <meta property="twitter:url" content="https://lacourt.dev/">
+  <meta property="twitter:title" content="David's Blog">
+  <meta property="twitter:description" content="David Lacourt's Blog. Posts about web technology, modern JavaScript frameworks, Agile methodologies, Software Craftmanship.">
+  <meta property="twitter:image" content="https://lacourt.dev/profile-pic.png">
 </svelte:head>
 
 <TitleBar level='h1' />
