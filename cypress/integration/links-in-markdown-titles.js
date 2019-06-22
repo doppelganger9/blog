d756b50db77e7484b9213ee75bd3dc5ee797e5a0
David LACOURT <david.lacourt@gmail.com>
2019-06-22 22:35:11 +0200

    Feature: add links & anchors to title <hX> from markdown.

---
+++ cypress/integration/links-in-markdown-titles.js
@@ -0,0 +1,38 @@
+let marked = require('marked');
+
+describe(`Links & Anchors in blog post markdown titles`, () => {
+
+  let renderer = new marked.Renderer();
+
+  beforeEach(() => {
+    // Override function
+    renderer.heading = function (text, level) {
+      var escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
+
+      return `
+          <h${level}>
+            <a name="${escapedText}" aria-hidden="true" class="anchor" href="#${escapedText}">
+              <span class="header-link"></span>
+            </a>
+            ${text}
+          </h${level}>`;
+    };
+  });
+
+  it(`should have an <A>nchor`, () => {
+    expect(marked('# heading+', { renderer: renderer })).to.contain('<a');
+  });
+
+  it(`should have a named <A>nchor, the name is escaped`, () => {
+    expect(marked('# escaped name', { renderer: renderer })).to.contain('name="escaped-name"');
+  });
+
+  it(`should have an <A>nchor with aria-hidden for a11y`, () => {
+    expect(marked('# escaped name', { renderer: renderer })).to.contain('aria-hidden="true"');
+  });
+
+  it(`should have an <A>nchor with href with #escaped-name`, () => {
+    expect(marked('# escaped name', { renderer: renderer })).to.contain('href="#escaped-name"');
+  });
+
+});
--- src/routes/_posts.js
+++ src/routes/_posts.js
@@ -15,6 +15,17 @@ export function getPosts () {
   });
 }
 
+const renderHeadingWithAnchor = (slug) => (text, level) => {
+  var escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
+  return `
+    <h${level}>
+      <a name="${escapedText}" aria-hidden="true" class="anchor" href="${slug}#${escapedText}">
+        <span class="header-link"></span>
+      </a>
+      ${text}
+    </h${level}>`;
+};
+
 export function getPost(slug) {
 
   const file = `${WHERE_ALL_THE_MARKDOWN_BLOG_POSTS_ARE}/${slug}.md`;
@@ -22,13 +33,20 @@ export function getPost(slug) {
 
   const markdown = fs.readFileSync(file, 'utf-8');
 
-  const { content, metadata } = process_markdown(markdown);
+  const { content, metadata } = processMarkdown(markdown);
 
   const date = new Date(metadata.date);
   const lang = 'en';
   metadata.dateString = date.toLocaleDateString(lang, { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })
 
-  const html = marked(content);
+  const renderer = new marked.Renderer();
+  renderer.heading = renderHeadingWithAnchor(metadata.slug);
+
+  const html = marked(content, {
+    headerIds: true,
+    smartypants: true,
+    renderer: renderer,
+  });
 
   const minutesToRead = computeMinutesToRead(content);
 
@@ -41,7 +59,7 @@ export function getPost(slug) {
   };
 }
 
-function process_markdown(markdown) {
+function processMarkdown(markdown) {
   const match = /---\n([\s\S]+?)\n---/.exec(markdown);
   const frontMatter = match[1];
   const content = markdown.slice(match[0].length);
