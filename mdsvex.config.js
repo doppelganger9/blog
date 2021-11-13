fce77ccba1159992a9c34077c3168ac4b97b3634
David LACOURT <david.lacourt+github@gmail.com>
2021-11-14 00:47:24 +0100

    totally inspired from blog post https://cass.moe/blog/svelte-kit-mdsvex to add remark and rehype plugins

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,8 +1,37 @@
+import abbr from "remark-abbr"
+import urls from "rehype-urls"
+import slug from "rehype-slug"
+import autoLinkHeadings from "rehype-autolink-headings"
+import addClasses from "rehype-add-classes"
+import figure from "rehype-figure"
+
+function processUrl(url, node) {
+	if (node.tagName === "a") {
+		node.properties.class = "text-link"
+
+		if (!url.href.startsWith("/")) {
+			// Open external links in new tab
+			node.properties.target = "_blank"
+			// Fix a security concern with offsite links
+			// See: https://web.dev/external-anchors-use-rel-noopener/
+			node.properties.rel = "noopener"
+		}
+	}
+}
+
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexConfig = {
   extensions: [ '.md', '.svx' ],
-  //remarkPlugins:[],
-  //rehypePlugins:[],
+  remarkPlugins: [
+    abbr // adds support for footnote-like abbreviations
+  ],
+  rehypePlugins:[
+    figure, // convert images into <figure> elements
+    [urls, processUrl], // adds rel and target to <a> elements
+    slug, // adds slug to <h1>-<h6> elements
+    [autoLinkHeadings, { behavior: "prepend" }], // adds a <a> around slugged <h1>-<h6> elements
+    [addClasses, { "ul,ol": "list" }] // add classes to these elements
+  ],
   //frontmatter: {},
   smartypants: true,
   //highlight: true, // il y a des options supplémentaires ici
