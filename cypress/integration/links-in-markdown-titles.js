37a8977bba55aff1ff31889733b1a7281a4713d6
David LACOURT <david.lacourt+github@gmail.com>
2021-11-14 20:30:35 +0100

    goodbye marked 👋

--- cypress/integration/links-in-markdown-titles.js
+++ cypress/integration/links-in-markdown-titles.js
@@ -2,41 +2,31 @@
 // https://on.cypress.io/intelligent-code-completion
 /// <reference types="cypress" />
 /// <reference types="../support" />
-let marked = require('marked').marked;
+
+import { force404 } from "../common";
 
 describe(`Links & Anchors in blog post markdown titles`, () => {
-
-  let renderer = new marked.Renderer();
-
   beforeEach(() => {
-    // Override function
-    renderer.heading = function (text, level) {
-      var escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
-
-      return `
-          <h${level}>
-            <a name="${escapedText}" aria-hidden="true" class="anchor" href="#${escapedText}">
-              <span class="header-link"></span>
-            </a>
-            ${text}
-          </h${level}>`;
-    };
+    force404(); // for the status indicator
+    cy.visitWithLang('/hello-world', 'en-US')
   });
 
   it(`should have an <A>nchor`, () => {
-    expect(marked('# heading+', { renderer: renderer })).to.contain('<a');
+    cy.getByDataCy('blog-post-heading').should('contain', `Hello`)
+    cy.get('h2 a').should('exist');
   });
 
-  it(`should have a named <A>nchor, the name is escaped`, () => {
-    expect(marked('# escaped name', { renderer: renderer })).to.contain('name="escaped-name"');
+  it(`should have an id on heading, the same as the href on the <A>nchor`, () => {
+    cy.get('h2#hi-').should('exist');
+    cy.get('h2#hi- a[href="#hi-"]').should('exist');
   });
 
   it(`should have an <A>nchor with aria-hidden for a11y`, () => {
-    expect(marked('# escaped name', { renderer: renderer })).to.contain('aria-hidden="true"');
+    cy.get('a[href="#hi-"]').should('exist').should("have.attr", "aria-hidden", "true");
   });
 
   it(`should have an <A>nchor with href with #escaped-name`, () => {
-    expect(marked('# escaped name', { renderer: renderer })).to.contain('href="#escaped-name"');
+    cy.get('#hi-').find('a').should('exist');
   });
 
 });
