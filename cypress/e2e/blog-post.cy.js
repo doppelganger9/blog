104c251e09d755737a1c1c698821bfc3aa701a96
David LACOURT <david.lacourt+github@gmail.com>
2023-01-16 21:55:14 +0100

    added some tests

--- cypress/e2e/blog-post.cy.js
+++ cypress/e2e/blog-post.cy.js
@@ -5,12 +5,14 @@
 import { titleBarShouldBeShown, builtByFooterShouldBeShown, articleFooterShouldBeShown, aBackLinkShouldBeShownTo, force404 } from '../common';
 
 describe(`a blog post`, () => {
-  
-  it(`should show the title bar in h3`, () => {
+  beforeEach(() => {
     force404(); // for the status indicator
     cy.visitWithLang('/', 'en-US');
     cy.wait(100); // if we don't wait, SvelteKit hydration will not work propertly. Yeah, I know, I'd prefer not to.
     cy.get(`a > h3`).last().click();
+  });
+
+  it(`should show the title bar in h3`, () => {
     titleBarShouldBeShown('h3')
   });
 
@@ -36,4 +38,24 @@ describe(`a blog post`, () => {
     builtByFooterShouldBeShown()
   });
 
+  it(`should have replaced all emojis with Twemojis in the content`, () => {
+    cy.get('.emoji').first().should('have.attr', 'src')
+      .should('include', 'https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/svg');
+  })
+
+  it(`should have links and ids on Level 2 headings`, () => {
+    cy.get('[data-cy=blog-post-content] h2').should('have.attr', 'id').get('a').should('have.attr', 'href');
+  })
+
+  it(`should have links and ids on Level 3 headings`, () => {
+    cy.get('[data-cy=blog-post-content] h3').should('have.attr', 'id').get('a').should('have.attr', 'href');
+  })
+
+  it(`should have links and ids on Level 4 headings`, () => {
+    cy.get('[data-cy=blog-post-content] h4').should('have.attr', 'id').get('a').should('have.attr', 'href');
+  })
+
+  it(`should have a giscus frame for comments`, () => {
+    cy.get('.giscus-frame').should('exist');
+  })
 });
