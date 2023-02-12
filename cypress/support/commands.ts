0063821981ed57a3d0801ab88766c8089bb12005
David LACOURT <david.lacourt+github@gmail.com>
2023-02-12 16:18:30 +0100

    fix types for command Cypress

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -83,36 +83,3 @@ Cypress.Commands.add("interceptGiscusAPI", () => {
       })
   }).as('mocked-giscus-getDiscussions-API');
 });
-
-declare namespace Cypress {
-    interface Chainable {
-      /**
-       * get an element targetting its `data-cy` attribute, as recommended by Cypress best practices.
-       * 
-       * @example
-       * cy.getByDataCy('main-heading')
-       * 
-       * @see 
-       * https://docs.cypress.io/guides/references/best-practices.html#Selecting-Elements
-       */
-      getByDataCy(selector: string): Chainable;
-
-      /**
-       * Override window.navigator.language to return some value.
-       * 
-       * @param target url to visit
-       * @param lang language to return when window.navigator.language is called
-       * @param options cy.visit() options
-       */
-      visitWithLang(target: string, lang: string, options?: any): Chainable;
-
-      /**
-       * Intercept and mock the Status API
-       * 
-       * @param status fake status to return
-       */
-      interceptStatusAPI(status: number): Chainable;
-
-      interceptGiscusAPI(): Chainable;
-    }
-  }
